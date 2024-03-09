3994b385965efb8d94d9f7d84b8469b8eafe8a01
ykofficial15 <yogesh15kumawat@gmail.com>
2024-03-09 21:46:46 +0530

    Own Laptop

---
+++ OwnLaptop.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+	double a, b;
+	char c;
+	cin >> a >> b >> c;
+	
+	if(c == '+')
+	{
+	    cout << a+b << endl;
+	}
+	else if(c == '-')
+	{
+	    cout << a-b << endl;
+	}
+	else if(c == '*')
+	{
+	    cout << a*b << endl;
+	}
+	else if(c == '/')
+	{
+	    cout << fixed << setprecision(7) << a/b << endl;
+	}
+	return 0;
+}
