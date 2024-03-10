7cafb48357db8ee1524e7d4080c50b4650037c5b
ykofficial15 <yogesh15kumawat@gmail.com>
2024-03-10 22:05:54 +0530

    FoodCosts Problem Done

---
+++ FoodCosts.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int a,b;
+cin>>a>>b;
+cout<<(a*6)+b<<endl;
+return 0;
+}
--- OwnLaptop.cpp
+++ OwnLaptop.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() 
-{
-	double a, b;
+{   double a, b;
 	char c;
 	cin >> a >> b >> c;
 	
