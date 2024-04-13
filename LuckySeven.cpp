4a54ebd37c28c45701329323f855dc7f0ad02cea
ykofficial15 <yogesh15kumawat@gmail.com>
2024-04-13 17:58:54 +0530

    another basic question

---
+++ LuckySeven.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string n;
+	cin>>n;
+	for(int i=0;i<n.length();i++)
+	{
+	    if(i==6)
+	    {
+	        cout<<n.at(i);
+	    }
+	}
+
+}
