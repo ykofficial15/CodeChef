141b421683b114510d1af3f4c4ba609b8120758a
ykofficial15 <yogesh15kumawat@gmail.com>
2024-04-28 21:15:43 +0530

    eventual rpoblem done

---
+++ StringManipulation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--) {
+	    int n, c;
+	    string s, ans = "YES";
+	    cin>> n >> s;
+	    
+	    if (n%2 != 0) ans = "NO";
+	    else {
+	        for (int i=0; i<n; i++) {
+	            c = count(s.begin(), s.end(), s[i]);
+	        
+	            if (c%2 != 0) {
+	                ans = "NO";
+	                break;
+	            }
+	        }
+	    }
+	    cout<< ans <<endl;
+	}
+	return 0;
+}
